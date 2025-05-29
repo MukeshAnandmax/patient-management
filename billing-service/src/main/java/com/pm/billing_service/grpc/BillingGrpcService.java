bcc38cbf767d94d73f483f80e6c4431048562a91
mukeshanandmax <mukeshanandmax@gmail.com>
2025-05-29 19:34:48 +0530

    Billing Service added, proto file created ,,Grpc server added.

---
+++ billing-service/src/main/java/com/pm/billing_service/grpc/BillingGrpcService.java
@@ -0,0 +1,27 @@
+package com.pm.billing_service.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase ;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@Slf4j
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest request, StreamObserver<billing.BillingResponse> responseObserver) {
+        //super.createBilling(request, responseObserver);
+        log.info("BillingRequest: {}", request.toString());
+
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("test")
+                .setStatus("success")
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+    }
+}
